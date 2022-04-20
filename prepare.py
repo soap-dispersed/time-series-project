3c5f9e2dfc9acc832342a657205d37c658edc2f7
Ben Smith <ben.f.smith07@gmail.com>
2022-04-20 10:55:46 -0500

    added train test split, edited prep_data()

--- prepare.py
+++ prepare.py
@@ -1,4 +1,30 @@
+from imports import *
+
 def prep_data(df):
+    
+    # rename columns: lower case, remove space
     for col in df.columns:
         df = df.rename(columns={col: col.lower().replace(' ', '_')})
+        
+    # cast date columns as datetime type
+    df.order_date = pd.to_datetime(df.order_date)
+    df.ship_date = pd.to_datetime(df.ship_date)
+        
+    # make datetime index
+    df.index = df.order_date
+    
+    # add profit per product column
+    df['profit_per_product'] = df.profit / df.quantity
+    
+    # add sales per product
+    df['sales_per_product'] = df.sales / df.quantity
+    
     return df
+
+def split_data(df):
+    '''
+    Splits data into train and test based on year.
+    '''
+    train = df['2014':'2016']
+    test = df['2017']
+    return train, test
